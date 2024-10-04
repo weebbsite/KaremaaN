d56f890e20d00ed691f7fe199871edbbcfeb5891
weebbsite <ma9223563@gmail.com>
2024-10-04 22:28:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector(".bar i");
+let list = document.querySelector("header ul");
+icon.addEventListener("click", function () {
+    list.classList.toggle("list")
+});
+
+let btn = document.querySelector("#daow")
